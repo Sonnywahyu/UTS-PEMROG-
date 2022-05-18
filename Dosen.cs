604b9b4472d2a6ad7abe485ecc67b06e6d0d5a2d
sonnywahyus <sonnywahyus@students.amikom.ac.id>
2022-05-18 20:17:58 +0700

    jawaban uts

---
+++ Dosen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dosen
+{
+    class Dosen
+    {
+        public int id;
+        public int NIK;
+        public string Nama, Gender, Course;
+        public void dosen()
+        {
+            Console.WriteLine("Dosen Pengampu : Kamarudin, M.Kom");
+            Console.WriteLine("Mata Kuliah : Pemrograman (ST021)");
+        }
+        public void attDosen(string nama, int nik, string gender)
+        {
+            Console.WriteLine("Dosen pengajar : {0}", nama);
+            Console.WriteLine("NIK Dosen : {0}", nik);
+            Console.WriteLine("Gender Dosen : {0}", gender);
+        }
+        public void addCourse(string course)
+        {
+            Console.WriteLine("Mata Kuliah : {0}", course);
+        }
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dosen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen Data1 = new Dosen();
+            Data1.dosen();
+            Console.Write("Masukkan nama dosen yang mengajar : ");
+            Data1.Nama = Console.ReadLine();
+            Console.Write("Masukkan NIK dosen yang mengajar : ");
+            Data1.NIK = Convert.ToInt32(Console.ReadLine());
+            Console.Write("input Gender dosen yang mengajar : ");
+            Data1.Gender = Console.ReadLine();
+            Console.Write("Masukkan mata kuliah yang diajarkan {0} : ", Data1.Nama);
+            Data1.Course = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Data Dosen");
+            Data1.dosen();
+            Data1.attDosen(Data1.Nama, Data1.NIK, Data1.Gender);
+            Data1.addCourse(Data1.Course);
+            Console.ReadLine();
+        }
+    }
+}
